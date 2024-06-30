bf71008d8fadacbb1248d0d8f80787b1afaea7dd
RichardGubijev <65620629+RichardGubijev@users.noreply.github.com>
2024-06-30 21:19:13 +0100

    Added strip_whitespace, fixed _get_category, added add_categories

--- data_preperation.py
+++ data_preperation.py
@@ -3,6 +3,8 @@ import bs4
 import os
 import numpy as np
 import html
+from web_scraper import save_as_json
+import re
 
 def load_json(filename):
     if os.path.exists(filename):
@@ -24,35 +26,46 @@ def _filter_out_dict(dictionary, filters):
     for k in keys:
         dictionary.pop(k)
 
-
 def extract_articles(DATA):
     pass
 
-def extract_text(HTML):
-    pass
+def _strip_whitespace(TEXT):
+    return re.sub(r"\s+", " ", TEXT)
+
+def extract_text(WEBPAGE):
+    soup = bs4.BeautifulSoup(WEBPAGE["HTML"], "html.parser")
+    return _strip_whitespace(soup.get_text())
 
 def _get_category(webpage):
     HTML = html.unescape(webpage["HTML"])
     webpage_parser = bs4.BeautifulSoup(HTML, "html.parser")
-    return str(webpage_parser.h1)[4:-5]
+    return str(webpage_parser.h1)[4:-5].replace("&amp", "&")
     
-
 def extract_categories(DATA):
-    keys = _filter_keys(DATA, "category")
+    keys = _filter_out_keys(DATA, "category")
     categories = {}
-    for k in keys[filter]:
+    for k in keys:
         category = _get_category(DATA[k])
         categories[category] = DATA[k]["extracted_URLs"]
-    print(categories)
     return categories
 
-def add_categories(DATA):
-    pass
-
+def add_categories(CATEGORIES, DATA):
+    for k in CATEGORIES.keys():
+        for a in CATEGORIES[k]:
+            if a in DATA:
+                if "category" not in DATA[a]:
+                    DATA[a]["category"] = [k]
+                else:
+                    DATA[a]["category"].append(k)
+                DATA[a]["category_num"] = len(DATA[a]["category"])
+                    
 
 def build_graph(DATA):
     pass
 
 
 DATA = load_json("webdata.json")
-extract_categories(DATA)
+print(extract_text(DATA["https://self-service.kcl.ac.uk/article/KA-01971/en-us"]))
+# categories = extract_categories(DATA)
+# add_categories(categories, DATA)
+# save_as_json(DATA, "webdata2.json")
